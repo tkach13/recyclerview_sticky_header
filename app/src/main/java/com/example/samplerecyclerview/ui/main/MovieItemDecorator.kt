a2966488beaa0164f08e373c542b55f71fcffc21
Tornike Kachkachishvili <tornikeka@gmail.com>
2020-01-26 02:04:19 +0400

    Bug fixed

--- app/src/main/java/com/example/samplerecyclerview/ui/main/MovieItemDecorator.kt
+++ app/src/main/java/com/example/samplerecyclerview/ui/main/MovieItemDecorator.kt
@@ -16,12 +16,9 @@ class MovieItemDecorator(@NonNull listener: StickyHeaderInterface) : RecyclerVie
         super.onDrawOver(c, parent, state)
         val topChild = parent.getChildAt(0) ?: return
         val topChildPosition = parent.getChildAdapterPosition(topChild)
-        println("topChildPosition = ${topChildPosition}")
-
         if (topChildPosition == RecyclerView.NO_POSITION) {
             return
         }
-
         val headerPosition = mlistener.getHeaderPositionForItem(topChildPosition)
         val currentHeader = getHeaderViewForItem(headerPosition, parent)
         fixLayoutSize(parent, currentHeader)
@@ -110,30 +107,13 @@ class MovieItemDecorator(@NonNull listener: StickyHeaderInterface) : RecyclerVie
 
     interface StickyHeaderInterface {
 
-        /**
-         * This method gets called by {@link StickHeaderItemDecoration} to fetch the position of the header item in the adapter
-         * that is used for (represents) item at specified position.
-         * @param itemPosition int. Adapter's position of the item for which to do the search of the position of the header item.
-         * @return int. Position of the header item in the adapter.
-         */
+
         fun getHeaderPositionForItem(itemPosition:Int):Int
-        /**
-         * This method gets called by {@link StickHeaderItemDecoration} to get layout resource id for the header item at specified adapter's position.
-         * @param headerPosition int. Position of the header item in the adapter.
-         * @return int. Layout resource id.
-         */
+
         fun getHeaderLayout(headerPosition:Int):Int
-        /**
-         * This method gets called by {@link StickHeaderItemDecoration} to setup the header View.
-         * @param header View. Header to set the data on.
-         * @param headerPosition int. Position of the header item in the adapter.
-         */
+
         fun bindHeaderData(header: View, headerPosition:Int)
-        /**
-         * This method gets called by {@link StickHeaderItemDecoration} to verify whether the item represents a header.
-         * @param itemPosition int.
-         * @return true, if item at the specified adapter's position represents a header.
-         */
+
         fun isHeader(itemPosition:Int):Boolean
 
     }
